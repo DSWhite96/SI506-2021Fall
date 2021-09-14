88ee3cb534442cc4b851cdf029753b914ffb6829
Derrick White <derrickswhite@outlook.com>
2021-09-14 17:00:20 -0400

    Adding PS_02 and LS_02

---
+++ problem_set_02/problem_set_02.py
@@ -0,0 +1,51 @@
+# START PROBLEM SET 02
+print('Problem Set 02')
+
+# PROBLEM 1 (10 points)
+print('\nProblem 1')
+
+gme_prices = [15.84, 35.50, 65.01, 325.00, 63.77]
+
+price_max = None #TODO: Replace
+
+price_max_index = None #TODO: Replace
+
+#TODO: Problem 1.3
+
+#TODO: Problem 1.4
+
+# PROBLEM 2 (10 points)
+print('\nProblem 2')
+
+amc_prices = None #TODO: Replace
+
+amc_prices_latest = None #TODO: Replace
+
+amc_prices_last_three = None #TODO: Replace
+
+# PROBLEM 3 (10 points)
+print('\nProblem 3')
+
+pltr_prices = ' 21.82-24.90-24.01-25.71-26.64-26.28 '
+
+pltr_prices_list = None #TODO: Replace
+
+# PROBLEM 4 (20 points)
+print('\nProblem 4')
+
+dates = ['September 10th', 'September 3rd', 'August 27th', 'August 20th', 'August 13th', 'August 6th']
+
+# PROBLEM 5 (20 points)
+print('\nProblem 5')
+
+#Uncomment once you've defined pltr_highest
+#print(pltr_highest)
+
+#Uncomment once you've defined amc_highest
+#print(amc_highest)
+
+# PROBLEM 6 (20 points)
+print('\nProblem 6')
+
+# PROBLEM 7 (10 points)
+print('\nProblem 7')
