f94291b84262a78d563dd8f6a7267fe762c29c74
Derrick White <derrickswhite@outlook.com>
2021-10-05 16:52:04 -0400

    PS05 and LE05

---
+++ problem_set_05/problem_set_05.py
@@ -0,0 +1,50 @@
+# SI 506 Problem Set 05
+
+import csv
+import copy
+
+print("Problem 01\n\n")
+
+# Problem 01: Implement read_csv and load the election data.
+
+# TODO Implement
+
+print("\n\nProblem 02\n\n")
+
+# Problem 02: Implement clean and clean the election data.
+
+# TODO Implement
+
+print("\n\nProblem 03\n\n")
+
+# Problem 03: Implement get_party_seat_differences and get the party seat differences for the 2021 election.
+
+# TODO Implement
+
+print("\n\nProblem 04\n\n")
+
+
+# Problem 04: Implement get_leaders and get the leaders for the 2021 election data.
+party_leaders_2021 = [
+                        ('AfD', 'Joerg Meuthen and Tino Chrupalla'),
+                        ('FDP', 'Christian Lindner'),
+                        ('CDU/CSU', 'Armin Laschet'),
+                        ('SPD', 'Olaf Scholz'),
+                        ('Greens', 'Annalena Baerbock and Robert Habeck'),
+                        ('Left', 'Janine Wissler and Susanne Hennig-Wellsow')
+                    ]
+
+# TODO Implement
+
+print("\n\nProblem 05\n\n")
+
+# Problem 05: Implement get_affiliation_percents and get affiliation percents for the 2021 election data.
+
+# TODO Implement
+
+print("\n\nProblem 06\n\n")
+
+
+# Problem 06: Implement write_csv and write election_data_2021_with_leaders to a file called revised_election_data_2021.csv.
+
+# TODO Implement
